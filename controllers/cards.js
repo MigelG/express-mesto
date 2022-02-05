1a41f6efd87c764162711d33f73270ec970c353e
MigelG <za4elovek@yandex.ru>
2022-02-05 16:51:42 +0300

    init

---
+++ controllers/cards.js
@@ -0,0 +1,59 @@
+const Card = require('../models/card');
+
+module.exports.getCards = (req, res) => {
+  Card.find({})
+    .then(cards => res.send({ data: cards }))
+    .catch(() => error(res, 500, 'Произошла ошибка'));
+};
+
+module.exports.createCard = (req, res) => {
+  const { name, link } = req.body;
+  Card.create({ name, link, owner: req.user._id })
+    .then(card => res.send({ data: card }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return error(res, 400, err.message);
+      }
+      return error(res, 500, 'Произошла ошибка');
+    });
+};
+
+module.exports.deleteCard = (req, res) => {
+  Card.findByIdAndRemove(req.params.id)
+    .then(card => checkResponse(res, card))
+    .catch((err) => handleCatch(res, err));
+};
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(req.params.cardId, {
+    $addToSet: { likes: req.user._id }
+  })
+    .then(card => checkResponse(res, card))
+    .catch((err) => handleCatch(res, err));
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(req.params.cardId, {
+    $pull: { likes: req.user._id }
+  })
+    .then(card => checkResponse(res, card))
+    .catch((err) => handleCatch(res, err));
+};
+
+function error(res, errorCode, message) {
+  return res.status(errorCode).send({ message: message });
+};
+
+function checkResponse(res, card) {
+  if (card) {
+    return res.send({ data: card });
+  }
+  return error(res, 404, 'Карточка не найдена');
+};
+
+function handleCatch(res, err) {
+  if (err.name === 'CastError') {
+    return error(res, 404, 'Карточка не найдена');
+  }
+  return error(res, 500, 'Произошла ошибка');
+}
